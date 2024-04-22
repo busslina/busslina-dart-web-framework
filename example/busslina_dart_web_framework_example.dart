6025606498a6e7dc06b3aa3605f351b55b28691d
Vicente <busslina@gmail.com>
2024-04-22 22:42:07 +0200

    Initial commit

---
+++ example/busslina_dart_web_framework_example.dart
@@ -0,0 +1,6 @@
+import 'package:busslina_dart_web_framework/busslina_dart_web_framework.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/busslina_dart_web_framework_test.dart
@@ -0,0 +1,16 @@
+import 'package:busslina_dart_web_framework/busslina_dart_web_framework.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
